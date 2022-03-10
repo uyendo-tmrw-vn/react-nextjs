68b478c7e424eeedefdb1848459e03ed36077451
uyendo-tmrw-vn <uyen.do@tmrw.vn>
2022-03-10 17:22:24 +0700

    cleare

--- src/store/actions/actionTypes.js
+++
@@ -1,2 +0,0 @@
-export const COUNTER_INCRIMENT = 'COUNTER_INCRIMENT'; //Will add one to counter
-export const COUNTER_DECRIMENT = 'COUNTER_DECRIMENT'; //Will subscruct one from counter
--- src/store/reducer/RootReducers.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux-immutable';
-import Immutable from 'immutable';
-import counterReducer from './counter';
-
-export const intitialState = Immutable.Map();
-
-export default combineReducers({
-    counter: counterReducer,
-  });
